8d97d2356e58cd7cbe529643761206c8bf3fba83
ana <anavikyvaquerovillarejo@gmail.com>
2024-07-31 19:49:08 -0600

    dropdown del navbar uwu

--- inc/head.php
+++ inc/head.php
@@ -5,8 +5,9 @@
     <link rel="stylesheet" href="Cem_estilo.css">
     <link rel="stylesheet" href="cem_estilo2.css">
     <link rel="stylesheet" href="cem_estilo3.css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <style>
         .btn-custom {
             background-color: #800000;
@@ -23,5 +24,11 @@
         }
         .padd{
             padding-left: 290px;
+        }
+        .red {
+            background-color: #780415; 
+        }
+        .link {
+        color: white;
         }
     </style>
--- inc/navbar.php
+++ inc/navbar.php
@@ -1,18 +1,32 @@
-    <nav class="navbar navbar-expand-lg navbar-custom">
-        <a class="navbar-brand" href="index.php?vista=home">CEM</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto padd">
-                <li class="nav-item"><a class="nav-link" href="index.php?vista=home"><strong>Inicio</strong></a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?vista=servicios">Servicios</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?vista=cotizar">Cotizar</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?vista=portafolio">Portafolio</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?vista=contacto">Contacto</a></li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a class="nav-link" href="index.php?vista=login">Perfil <img src="img/foto_perfil.jpg" alt="Foto de perfil" class="profile-img"></a></li>
-            </ul>
+<nav class="navbar navbar-expand-lg navbar-custom">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php?vista=home">CEM</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto padd">
+                    <li class="nav-item"><a class="nav-link" href="index.php?vista=home"><strong>Inicio</strong></a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?vista=servicios">Servicios</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?vista=cotizar">Cotizar</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?vista=portafolio">Portafolio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?vista=contacto">Contacto</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu red navbar" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item link" href="#">Administrador</a>
+                            <a class="dropdown-item link" href="#">Aparatos</a>
+                            <a class="dropdown-item link" href="#">Cotizaciones</a>
+                            <a class="dropdown-item link" href="#">Contenido</a>
+                            <a class="dropdown-item link" href="#">Roles</a>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.php?vista=login">Perfil <img src="img/foto_perfil.jpg" alt="Foto de perfil" class="profile-img"></a></li>
+                </ul>
+            </div>
         </div>
     </nav>
