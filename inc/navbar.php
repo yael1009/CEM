ccde295fd1d0c03ea8429a392df00d1469fbf05a
yael1009 <emilianozapataypanchovilla@gmail.com>
2024-08-06 10:28:25 -0600

    acomode mejor roles, quite archivos obsoletos, ajuste navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -6,7 +6,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav mx-auto padd">
-                    <li class="nav-item"><a class="nav-link" href="index.php?vista=home"><strong>Inicio</strong></a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?vista=home">Inicio</a></li>
                     <li class="nav-item"><a class="nav-link" href="index.php?vista=servicios">Servicios</a></li>
                     <li class="nav-item"><a class="nav-link" href="index.php?vista=cotizar">Cotizar</a></li>
                     <li class="nav-item"><a class="nav-link" href="index.php?vista=portafolio">Portafolio</a></li>
@@ -15,15 +15,35 @@
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Administrar
                         </a>
-                        <div class="dropdown-menu red" aria-labelledby="navbarDropdownMenuLink">
+                        <div class="dropdown-menu darkeRed" aria-labelledby="navbarDropdownMenuLink">
+                        <div class="darkeRed">
                             <a class="dropdown-item link" href="index.php?vista=administrador">Administrador</a>
-                            <a class="dropdown-item link" href="index.php?vista=aparatos">Aparatos</a>
-                            <a class="dropdown-item link" href="index.php?vista=cotizaciones">Cotizaciones</a>
-                            <a class="dropdown-item link" href="index.php?vista=contenido">Contenido</a>
-                            <a class="dropdown-item link" href="index.php?vista=roles">Roles</a>
                         </div>
-                    </li>
-                </ul>
+                        <div class="red">
+                            <a class="dropdown-item link" href="index.php?vista=aparatos">Aparatos</a>
+                        </div>
+                        <hr class="dropdown-divider">
+                        <div class="darkeRed">
+                            <a class="dropdown-item link" href="index.php?vista=cotizaciones">Cotizaciones</a>
+                        </div>
+                        <div class="red">
+                            <a class="dropdown-item link" href="roles_clientes.html">Solicitudes</a>
+                            <a class="dropdown-item link" href="roles_clientes.html">En Progreso</a>
+                            <a class="dropdown-item link" href="roles_clientes.html">Completado</a>
+                            <a class="dropdown-item link" href="roles_clientes.html">Cancelado</a>
+                        </div>
+                        <hr class="dropdown-divider">
+                        <div class="darkeRed">
+                            <a class="dropdown-item link" href="dropdowns.html">Roles</a>
+                        </div>
+                        <div class="red">
+                            <a class="dropdown-item link" href="index.php?vista=clientes">Clientes</a>
+                            <a class="dropdown-item link" href="index.php?vista=empleados">Personal</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
 
                 <?php
                 // Verificar si la sesión está iniciada
@@ -42,6 +62,5 @@
                     </ul>';
                 }
                 ?>
-            </div>
         </div>
     </nav>
---
+++ views/clientes.php
@@ -0,0 +1,213 @@
+<div class="container mt-4">
+    <h1 class="text-center">Usuarios Clientes</h1>
+
+    <div class="service-divider"></div>
+
+
+    <div class="profile-container mt-4">
+    <?php
+        require_once "./class/main.php";
+        $main = new main();
+
+        if(isset($_POST['modulo_buscador'])){
+            require_once "./scripts/buscador.php";
+        }
+
+        if(!isset($_SESSION['busqueda_usuario']) && empty($_SESSION['busqueda_usuario'])){
+    ?>
+    <div class="columns">
+        <div class="column">
+            <form action="" method="POST" autocomplete="off" >
+                <input type="hidden" name="modulo_buscador" value="usuario">   
+                <div class="field is-grouped">
+                    <p class="control is-expanded">
+                        <input class="form-control" type="text" name="txt_buscador" placeholder="¿Qué estas buscando?" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]{1,30}" maxlength="30" >
+                    </p>
+                    <p class="control">
+                        <button class="btn btn-custom" type="submit" >Buscar</button>
+                    </p>
+                </div>
+            </form>
+        </div>
+    </div>
+    <?php }else{ ?>
+    <div class="columns">
+        <div class="column">
+            <form class="has-text-centered mt-6 mb-6" action="" method="POST" autocomplete="off" >
+                <input type="hidden" name="modulo_buscador" value="usuario"> 
+                <input type="hidden" name="eliminar_buscador" value="usuario">
+                <p>Estas buscando <strong>“<?php echo $_SESSION['busqueda_usuario']; ?>”</strong></p>
+                <br>
+                <button type="submit" class="btn btn-custom">Eliminar busqueda</button>
+            </form>
+        </div>
+    </div>
+    <?php
+            $busqueda=$_SESSION['busqueda_usuario']; /* <== */
+
+        }
+            # Eliminar usuario #
+            /*if(isset($_GET['user_id_del'])){
+                require_once "./php/usuario_eliminar.php";
+            }*/
+
+            if(!isset($_GET['page'])){
+                $pagina=1;
+            }else{
+                $pagina=(int) $_GET['page'];
+                if($pagina<=1){
+                    $pagina=1;
+                }
+            }
+
+            $pagina=$main->limpiarstring($pagina);
+            $url="index.php?vista=roles&page="; /* <== */
+            $registros=5;
+
+            # Paginador usuario #
+            require_once "./scripts/usuario_lista.php";
+    ?>
+    </div>
+</div>
+
+<!-- Expandir -->
+<div class="modal fade" id="SeeMoreUser" tabindex="-1" aria-labelledby="userModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2 class="modal-title" id="userModalLabel">Nombre Usuario</h2>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row centerr">
+                    <img src="../img/foto_perfil_clientes.jpg" width="30%" alt="Imagen">
+                </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="table-custom">
+                                <div class="table-header p-2">
+                                    Datos Personales
+                                </div>
+                                <table class="table mb-0" >
+                                    <tbody>
+                                        <tr>
+                                            <th>Nombres:</th>
+                                            <td>Jorge</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Apellido Paterno:</th>
+                                            <td>Fabela</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Apellido Materno:</th>
+                                            <td>No se</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Telefono:</th>
+                                            <td>871 666 6969</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Correo:</th>
+                                            <td>jorgito_uwu@gmail.com</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <div class="table-custom">
+                                    <div class="table-header p-2">
+                                        Seleccione los roles
+                                    </div>
+                                    <table class="table mb-0" >
+                                        <tbody>
+                                        <div class="form-check">
+                                            <tr>
+                                                <th></th>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="cliente">
+                                                    <label class="form-check-label" for="cliente">
+                                                    Cliente
+                                                </label></td>
+                                            </tr>
+                                        </div>
+                                        <div class="form-check">
+                                            <tr>
+                                                <th></th>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="gestorContenido">
+                                                    <label class="form-check-label" for="gestorContenido">
+                                                    Gestor de Contenido
+                                                </label></td>
+                                            </tr>
+                                        </div>
+                                        <div class="form-check">
+                                            <tr>
+                                                <th></th>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="gestorRoles">
+                                                    <label class="form-check-label" for="gestorRoles">
+                                                    Gestor de Roles
+                                                </label></td>
+                                            </tr>
+                                        </div>
+                                        <div class="form-check">
+                                            <tr>
+                                                <th></th>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="gestorCotizacion">
+                                                    <label class="form-check-label" for="gestorCotizacion">
+                                                    Gestor de Cotización
+                                                </label></td>
+                                            </tr>
+                                        </div>
+                                        <div class="form-check">
+                                            <tr>
+                                                <th></th>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="administrador">
+                                                    <label class="form-check-label" for="administrador">
+                                                    Administrador
+                                                </label></td>
+                                            </tr>
+                                        </div>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="table-custom">
+                                <div class="table-header p-2">
+                                    Datos de Usuario
+                                </div>
+                                <table class="table mb-0" >
+                                    <tbody>
+                                        <tr>
+                                            <th>Usuario:</th>
+                                            <td>jorgito_uwu</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Contraseña:</th>
+                                            <td>*************</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Uso de la cuenta:</th>
+                                            <td>Personal</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Cargo de la compañía:</th>
+                                            <td>Propietario</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger">Eliminar</button>
+                    <button type="button" class="btn btn-info">Editar</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Salir</button>
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>    
